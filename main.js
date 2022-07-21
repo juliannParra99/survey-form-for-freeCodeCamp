5f16012421ed729bb617bc85722d6f23a2066a00
julianParrax <julianfranco.parra@gmail.com>
2022-07-21 18:28:59 -0300

    JS added and slight improvements

---
+++ main.js
@@ -0,0 +1,11 @@
+const subBtn = document.querySelector('.submit-buttom');
+
+
+subBtn.addEventListener('click', sendForm);
+
+function sendForm(){
+  let send = confirm('Are you sure you want to submit the form?');
+  let success = 'Submitted successfully!'; 
+  send?  alert(success) : false;
+  subBtn.removeEventListener('click',sendForm)
+}
